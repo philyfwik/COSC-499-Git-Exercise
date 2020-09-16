e2b44e4d8744231cea0648e4b0816187635dd48c
Philip Okanlawon <philyfwik@Philys-MBP.hitronhub.home>
2020-09-15 22:36:09 -0700

    comment

--- GitEx.c
+++ GitEx.c
@@ -3,7 +3,7 @@
 int user(int num);
 int main()
 {   
-    printf("Hello there!"); // new comment here
+    printf("Hello there!"); // Change to program
 
     char fname[15];
     printf("\n\nEnter your first name: ");
