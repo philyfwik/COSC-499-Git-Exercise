322575bea49f7087f545c450882c6d0a97ac44ee
Philip Okanlawon <philyfwik@Philys-MBP.hitronhub.home>
2020-09-15 22:03:31 -0700

    feature(s) 2

--- GitEx.c
+++ GitEx.c
@@ -3,5 +3,15 @@ int main()
 {   
     printf("Hello there!");
 
+    char fname[15];
+    printf("\n\nEnter your first name: ");
+    scanf("%s", fname);
+
+    char lname[15];
+    printf("\nEnter your last name: ");
+    scanf("%s", lname);
+
+    printf("\nWelcome %s %s! I am here to let you know if you're legal here or not!\n", fname, lname);
+
     return 0;
 }
